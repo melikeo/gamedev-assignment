634f1f0a3e85bb45845f2bc2e894a0b1b665b6b2
melikeo <98155324+melikeo@users.noreply.github.com>
2024-11-20 19:51:19 +1100

    restructuring level2 script for new approach

--- Assets/Scripts/Level2.cs
+++ Assets/Scripts/Level2.cs
@@ -5,16 +5,15 @@ using UnityEngine.Tilemaps;
 
 public class Level2 : MonoBehaviour
 {
-    public GameObject ghost3Prefab; //reference to prefab
-    //public CountdownManager countdownManager; // reference to CountdownManager
 
-    private float spawnInterval = 10; // create a new random walking ghosts every 10 seconds
+    public GameObject[] ghosts; // Array of the new ghosts that will be activated every interval seconds
 
+    private float timeInterval = 30f;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(CreateNewGhosts());
+        StartCoroutine(ActivateNewGhost());
     }
 
     // Update is called once per frame
@@ -23,47 +22,11 @@ public class Level2 : MonoBehaviour
 
     }
 
-    IEnumerator CreateNewGhosts()
+    IEnumerator ActivateNewGhost()
     {
-        while (true)
-        {
-            yield return new WaitForSeconds(spawnInterval); // create new ghosts every x seconds
+        yield return new WaitForSeconds(timeInterval);
 
-            GameObject newGhostInstance = Instantiate(ghost3Prefab, new Vector3(-5.5f, -5, 0), Quaternion.identity); // spawn at Ghost 3 position
-
-            //countdownManager.newGhost = newGhostInstance;
-
-            //if (countdownManager != null)
-            //{
-            //    countdownManager.newGhost = newGhostInstance;
-            //}
-
-            GhostController ghostController = newGhostInstance.GetComponent<GhostController>(); // get ghost controller
-
-            if (ghostController != null)
-            {
-                ghostController.topLeftTilemap = FindTilemap("TopLeft");
-                ghostController.topRightTilemap = FindTilemap("TopRight");
-                ghostController.bottomLeftTilemap = FindTilemap("BottomLeft");
-                ghostController.bottomRightTilemap = FindTilemap("BottomRight");
-
-                Debug.Log("Tilemaps referencing worked!");
-            }
-
-            Debug.Log("code running");
-        }
     }
-
-    Tilemap FindTilemap(string name)
-    {
-        GameObject tilemapObject = GameObject.Find(name); // search for tilemap
-        if (tilemapObject != null)
-        {
-            return tilemapObject.GetComponent<Tilemap>(); // returns Tilemap Object
-        }
-        //Debug.LogWarning($"Tilemap '{name}' not found!!");
-        return null; // found nothing
-    }
-
+  
 
 }
